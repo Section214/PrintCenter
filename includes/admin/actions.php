c27ad2b56b8cdd8adf43c873241d5b5195481391
Ghost1227 <dgriffiths@section214.com>
2015-07-26 14:56:11 -0500

    Re-add admin files

---
+++ includes/admin/actions.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Admin actions
+ *
+ * @package     PrintCenter\Admin\Actions
+ * @since       1.0.0
+ */
+
+
+// Exit if accessed directly
+if( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
+/**
+ * Process all actions sent via POST and GET by looking for the 'printcenter-action'
+ * request and running do_action() to call the function
+ *
+ * @since       1.0.0
+ * @return      void
+ */
+function printcenter_process_actions() {
+    if( isset( $_POST['printcenter-action'] ) ) {
+        do_action( 'printcenter_' . $_POST['printcenter-action'], $_POST );
+    }
+
+    if( isset( $_GET['printcenter-action'] ) ) {
+        do_action( 'printcenter_' . $_GET['printcenter-action'], $_GET );
+    }
+}
+add_action( 'admin_init', 'printcenter_process_actions' );
---
+++ includes/admin/pages.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Admin pages
+ *
+ * @package     PrintCenter\Admin\Pages
+ * @since       1.0.0
+ */
+
+
+// Exit if accessed directly
+if( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
+/**
+ * Create the settings menu pages
+ *
+ * @since       1.0.0
+ * @global      string $printcenter_settings_page The PrintCenter settings page hook
+ * @return      void
+ */
+function printcenter_add_settings_pages() {
+    global $printcenter_settings_page;
+
+    $printcenter_settings_page = add_menu_page( __( 'PrintCenter Settings', 'printcenter' ), __( 'PrintCenter', 'printcenter' ), 'manage_options', 'printcenter-settings', 'printcenter_render_settings_page' );
+}
+add_action( 'admin_menu', 'printcenter_add_settings_pages', 10 );
+
+
+/**
+ * Determines whether or not the current admin page is a PrintCenter page
+ *
+ * @since       1.0.0
+ * @param       string $hook The hook for this page
+ * @global      string $typenow The post type we are viewing
+ * @global      string $pagenow The page we are viewing
+ * @global      string $printcenter_settings_page The PrintCenter settings page hook
+ * @return      bool $ret True if PrintCenter page, false otherwise
+ */
+function printcenter_is_admin_page( $hook ) {
+    global $typenow, $pagenow, $printcenter_settings_page;
+
+    $ret    = false;
+    $pages  = apply_filters( 'printcenter_admin_pages', array( $printcenter_settings_page ) );
+
+    if( in_array( $hook, $pages ) ) {
+        $ret = true;
+    }
+
+    return (bool) apply_filters( 'printcenter_is_admin_page', $ret );
+}
