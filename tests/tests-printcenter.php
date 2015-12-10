19f48510a33e1cadd53ba1a67b128ccbd8783b4f
Ghost1227 <dgriffiths@section214.com>
2015-12-09 21:11:40 -0700

    Add core tests

---
+++ tests/tests-printcenter.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class Tests_PrintCenter extends WP_UnitTestCase {
+	protected $object;
+
+	public function setUp() {
+		parent::setUp();
+		$this->object = origami();
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+	public function test_printcenter_instance() {
+		$this->assertClassHasStaticAttribute( 'instance', 'PrintCenter' );
+		$this->assertClassHasAttribute( 'loader', 'PrintCenter' );
+	}
+}
