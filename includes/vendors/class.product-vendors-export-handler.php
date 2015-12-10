0e97655686df24d49cf3d39a7fe85233dd416471
Ghost1227 <dgriffiths@section214.com>
2015-12-09 17:00:02 -0700

    Code cleanup

--- includes/vendors/class.product-vendors-export-handler.php
+++ includes/vendors/class.product-vendors-export-handler.php
@@ -1,13 +1,19 @@
 <?php
+/**
+ * Bootstraps the Vendor system
+ *
+ * @package     PrintCenter\Vendor
+ * @since       1.0.0
+ */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+// Exit if accessed directly
+if( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * Product Vendors Import/Export Handler
- *
- * Adds support for:
- *
- * + Customer / Order CSV Export
  *
  * @since 1.0.0
  */
@@ -17,12 +23,12 @@ class WooCommerce_Product_Vendors_Export_Handler {
 	/**
 	 * Setup class
 	 *
-	 * @since 1.1.5
+	 * @access      public
+	 * @since       1.0.0
 	 */
 	public function __construct() {
-
 		// Customer / Order CSV Export column headers/data
-		add_filter( 'wc_customer_order_csv_export_order_headers',              array( $this, 'add_vendors_to_csv_export_column_headers' ), 10, 2 );
+		add_filter( 'wc_customer_order_csv_export_order_headers', array( $this, 'add_vendors_to_csv_export_column_headers' ), 10, 2 );
 		add_filter( 'wc_customer_order_csv_export_order_row_one_row_per_item', array( $this, 'add_vendors_to_csv_export_column_data' ), 10, 4 );
 
 		// Customer / Order CSV Export line item data
@@ -31,15 +37,15 @@ class WooCommerce_Product_Vendors_Export_Handler {
 
 
 	/**
-	 * Adds support for Customer/Order CSV Export by adding a vendor column
-	 * header
+	 * Adds support for Customer/Order CSV Export by adding a vendor column header
 	 *
-	 * @since 1.0.0
-	 * @param array $headers existing array of header key/names for the CSV export
-	 * @return array
+	 * @access      public
+	 * @since       1.0.0
+	 * @param       array $headers existing array of header key/names for the CSV export
+	 * @param       WC_Customer_Order_CSV_Export_Generator $csv_generator instance
+	 * @return      array
 	 */
 	public function add_vendors_to_csv_export_column_headers( $headers, $csv_generator ) {
-
 		$headers['vendors'] = 'vendors';
 
 		return $headers;
@@ -50,20 +56,19 @@ class WooCommerce_Product_Vendors_Export_Handler {
 	 * Adds support for Customer/Order CSV Export by adding product vendor data
 	 * for each order item row
 	 *
-	 * @since 1.0.0
-	 * @param array $order_data generated order data matching the column keys in the header
-	 * @param WC_Order $order order being exported
-	 * @param \WC_Customer_Order_CSV_Export_Generator $csv_generator instance
-	 * @return array
+	 * @access      public
+	 * @since       1.0.0
+	 * @param       array $order_data generated order data matching the column keys in the header
+	 * @param       array $item data for the specific item
+	 * @param       WC_Order $order order being exported
+	 * @param       WC_Customer_Order_CSV_Export_Generator $csv_generator instance
+	 * @return      array
 	 */
 	public function add_vendors_to_csv_export_column_data( $order_data, $item, $order, $csv_generator ) {
-
 		$vendors = array();
 
 		if ( isset( $item['id'] ) && $product_id = $item['id'] ) {
-
 			if ( $product_vendors = get_product_vendors( $product_id ) ) {
-
 				foreach ( (array) $product_vendors as $vendor ) {
 					$vendors[] = $vendor->title;
 				}
@@ -80,21 +85,20 @@ class WooCommerce_Product_Vendors_Export_Handler {
 	 * Filter the individual line item entry to add product id for older versions
 	 * of Customer/Order CSV Export
 	 *
-	 * @since 1.0.0
-	 * @param array $line_item line item data in key => value format
-	 * @param array $item WC order item data
-	 * @param WC_Product $product the product
-	 * @param WC_Order $order the order
-	 * @param \WC_Customer_Order_CSV_Export_Generator $csv_generator instance
-	 * @return array
+	 * @access      public
+	 * @since       1.0.0
+	 * @param       array $line_item line item data in key => value format
+	 * @param       array $item WC order item data
+	 * @param       WC_Product $product the product
+	 * @param       WC_Order $order the order
+	 * @param       WC_Customer_Order_CSV_Export_Generator $csv_generator instance
+	 * @return      array
 	 */
 	public function csv_export_line_item_data( $line_item, $item, $product, $order, $csv_generator ) {
-
 		if ( isset( $csv_generator->order_format ) && ( 'default_one_row_per_item' == $csv_generator->order_format || 'legacy_one_row_per_item' == $csv_generator->order_format ) ) {
 			$line_item['id'] = $product->id;
 		}
 
 		return $line_item;
 	}
-
-} // end WooCommerce_Product_Vendors_Export_Handler
+}
